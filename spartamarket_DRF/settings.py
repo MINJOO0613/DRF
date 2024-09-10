0bb41ea72afb72bf58cfc0bafdb4dce8a3b20ccc
KIM MIN JOO <rlaalswn0613@gmail.com>
2024-09-10 16:38:51 +0900

    feat: 시크릿키 gitignore처리함. 최종 제출

--- spartamarket_DRF/settings.py
+++ spartamarket_DRF/settings.py
@@ -21,7 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-)*p0yovp7f+x=y&s$jbq=l^g%r6)h7g(t*_h51rhsy&o)%)=)3"
+from . import config
+SECRET_KEY = config.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
